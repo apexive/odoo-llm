ce17abcd4f96af9fbb4202f8dccdb8452f3413ee
RobertPechaCZ <bofteam@centrum.cz>
2025-06-06 18:22:20 +0200

    wip

--- llm_thread/static/src/models/composer.js
+++ llm_thread/static/src/models/composer.js
@@ -29,7 +29,7 @@ registerPatch({
       // This should close event source
       this._closeEventSource();
     },
-    async postUserMessageForLLM() {
+    postUserMessageForLLM() {
       const thread = this.thread;
 
       const messageBody = this.textInputContent.trim();
@@ -43,12 +43,28 @@ registerPatch({
 
       this._reset();
 
+      this._handleResponsesFromLLM(thread, `/llm/thread/generate?thread_id=${thread.id}&message=${encodeURIComponent(messageBody)}`);
+    },
+
+    repostExistingUserMessagesForLLM() {
+      const thread = this.thread;
+
+      if (!thread) {
+        this.messaging.notify({
+          message: this.env._t("Thread not found."),
+          type: "danger",
+        });
+        return;
+      }
+
+      this._reset();
+
+      this._handleResponsesFromLLM(thread, `/llm/thread/generate?thread_id=${thread.id}`);
+    },
+
+    handleResponsesFromLLM(thread, url) {
       try {
-        const eventSource = new EventSource(
-          `/llm/thread/generate?thread_id=${
-            thread.id
-          }&message=${encodeURIComponent(messageBody)}`
-        );
+        const eventSource = new EventSource(url);
         this.update({ eventSource });
 
         eventSource.onmessage = async (event) => {
