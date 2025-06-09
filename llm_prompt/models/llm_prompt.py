af68874302b0dc56f2305fc7fdfa9aa990b14d99
theBeardA <35360097+theBeardA@users.noreply.github.com>
2025-06-09 15:24:38 -0700

    add logs

--- llm_prompt/models/llm_prompt.py
+++ llm_prompt/models/llm_prompt.py
@@ -238,6 +238,8 @@ class LLMPrompt(models.Model):
         self.ensure_one()
         arguments = arguments or {}
 
+        print("get messages:", arguments)
+
         # Fill default values for missing arguments
         arguments = self._fill_default_values(arguments)
 
@@ -293,6 +295,8 @@ class LLMPrompt(models.Model):
             ValidationError: If arguments are invalid
         """
         self.ensure_one()
+
+        print("Validating arguments:", arguments)
 
         try:
             schema = json.loads(self.arguments_json or "{}")
